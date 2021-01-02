331b185bd0d02a3ae0fd698a3c21299a04c98676
Miguel Hernanz <migherdcas@gmail.com>
2021-01-02 23:24:28 +0100

    feat(React): Redux file structure.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css';
+import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import store from '../store/store';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      {' '}
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
