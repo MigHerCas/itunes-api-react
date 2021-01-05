6036e1074333e48b0e7d9cc23d0d21a3fb2178a4
Miguel Hernanz <migherdcas@gmail.com>
2021-01-05 20:45:18 +0100

    feat(Next): Images domains added.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     BASE_API_URL: "https://itunes.apple.com/search",
   },
   images: {
-    domains: ['unsplash.it'],
+    domains: ['unsplash.it', 'is1-ssl.mzstatic.com', 'is2-ssl.mzstatic.com', 'is3-ssl.mzstatic.com', 'is4-ssl.mzstatic.com', 'is5-ssl.mzstatic.com'],
   },
 };
