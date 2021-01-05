7e488fc225754f5047af5a9f211360bb880fd1f7
Miguel Hernanz <migherdcas@gmail.com>
2021-01-05 11:15:57 +0100

    feat(Redux): Store provider wraps Pages now.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { useStore } from '../store';
 import '../scss/styles.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  // Initialize Store
+  const store = useStore(pageProps.initialReduxState);
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
