ca92cacf39a535ca255d5842b18d91c5a4f8431a
Miguel Hernanz <migherdcas@gmail.com>
2021-01-07 18:13:01 +0100

    fix(React): Unneeded check removed.

--- src/containers/ItunesItems.tsx
+++ src/containers/ItunesItems.tsx
@@ -6,36 +6,26 @@ import ItunesItem from '../components/ItunesItem';
 export default function ItunesItems(): JSX.Element {
   const { itunesItems } = useStoreItems();
 
-  if (!itunesItems.length) return <EmptyResults />;
+  if (!itunesItems || !itunesItems.length) return <EmptyResults />;
 
   return (
     <ol className="grid itunes-items">
-      {itunesItems &&
-        itunesItems.map((itunesItem) => {
-          const {
-            artistId,
-            artistName,
-            id,
-            imgUrl,
-            isFavourite,
-            title,
-            type,
-            onToggle,
-          } = itunesItem;
-          return (
-            <ItunesItem
-              key={id}
-              id={id}
-              title={title}
-              artistName={artistName}
-              artistId={artistId}
-              isFavourite={isFavourite}
-              type={type}
-              imgUrl={imgUrl}
-              onToggle={onToggle}
-            />
-          );
-        })}
+      {itunesItems.map((itunesItem) => {
+        const { artistId, artistName, id, imgUrl, isFavourite, title, type, onToggle } = itunesItem;
+        return (
+          <ItunesItem
+            key={id}
+            id={id}
+            title={title}
+            artistName={artistName}
+            artistId={artistId}
+            isFavourite={isFavourite}
+            type={type}
+            imgUrl={imgUrl}
+            onToggle={onToggle}
+          />
+        );
+      })}
     </ol>
   );
 }
